bce240ca296cabc118e479d1ac6edba802a7c13c
Freddie Nelson <freddie0208@hotmail.com>
2022-05-30 20:29:42 +0100

    fix: checkbox value not settings to initial modelValue value

--- src/components/Input/SInputCheckbox/SInputCheckbox.vue
+++ src/components/Input/SInputCheckbox/SInputCheckbox.vue
@@ -18,9 +18,13 @@ export default defineComponent({
     const checked = ref(false);
     const modelValue = computed(() => props.modelValue);
 
-    watch(modelValue, () => {
-      checked.value = props.modelValue;
-    });
+    watch(
+      modelValue,
+      () => {
+        checked.value = modelValue.value;
+      },
+      { immediate: true }
+    );
 
     const inputAttrs = computed(() => {
       const a = { ...attrs, class: "" };
@@ -58,8 +62,12 @@ export default defineComponent({
 
     <button
       v-bind="buttonAttrs"
-      :class="checked ? 'bg-primary-400' : 'bg-neutral-300'"
-      class="w-9 h-9 p-1.5 rounded-md text-neutral-50 focus:outline-2 focus:outline-neutral-600 transition-colors duration-300"
+      :class="
+        checked
+          ? 'bg-primary-400 border-primary-400'
+          : 'bg-neutral-50 border-neutral-300'
+      "
+      class="w-9 h-9 p-1.5 rounded-md text-neutral-50 focus:outline-2 focus:outline-neutral-600 transition-colors duration-300 border-4 relative flex items-center justify-center"
       type="button"
       @click="
         checked = !checked;
@@ -67,7 +75,7 @@ export default defineComponent({
       "
     >
       <Icon
-        class="w-full h-full transition-opacity duration-300"
+        class="absolute p-0.5 h-full w-full transition-opacity duration-300"
         :class="{ 'opacity-full': checked, 'opacity-0': !checked }"
         :icon="icons.tick"
       />
